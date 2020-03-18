4a71bb385fbe0b9d13f8cc42f67a0bb9d122084a
youngil <mycode01@hotmail.com>
2020-03-18 18:01:43 +0900

    init

---
+++ README.md
@@ -0,0 +1,10 @@
+# vue-storybook
+
+## setup and run storybook
+```
+npm install
+npm run storybook:serve
+```
+
+### Customize configuration
+See [Configuration Reference](https://cli.vuejs.org/config/).
---
+++ src/views/Home.vue
@@ -0,0 +1,16 @@
+<template lang="pug">
+  div.home
+    HelloWorld(msg="Welcome to Your Vue.js App")
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from "@/components/HelloWorld/HelloWorld.vue";
+
+export default {
+  name: "home",
+  components: {
+    HelloWorld
+  }
+};
+</script>
